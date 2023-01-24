c7c607b94251e8d2c18e85dd57c397c77bcb0f2c
Bavurhe <sorayabav95@gmail.com>
2023-01-24 00:53:51 -0800

    fixing betty

--- print_dec.c
+++ print_dec.c
@@ -1,43 +1,41 @@
 #include "main.h"
+
 /**
- * print_dec - prints a given integer from a variadic paramter to base 10
- * @arguments: variadic paramter
+ * print_dec - prints a given integer from a variadic parameter to base 10
+ * @arguments: variadic parameter
  * Return: number of character printed on success
 */
-
 int print_dec(va_list arguments)
 {
-int i = 0;
-int count = 0;
-int numb;
-int my_arr[10];
-char x[1];
+	int i = 0;
+	int count = 0;
+	int numb;
+	int my_arr[10];
+	char x[1];
 
-/*Fetch integer from variadic paramters*/
-numb = va_arg(arguments, int);
+	/*Fetch integer from variadic parameters*/
+	numb = va_arg(arguments, int);
 
-/*Break the anumber apart and assign them to array*/
-while (numb != 0)
-{
-my_arr[i] = (numb % 10);
-numb = numb / 10;
-if (numb == 0)
-{
-break;
-}
-else
-{
-i++;
-}
-}
-
-/*Write the content of the array to stdout*/
-for (i; i >= 0; i--)
-{
-/*Convert int to char*/
-x[0] = ('0' + my_arr[i]);
-count += write(1, x, 1);
-}
-
-return (count);
+	/*Break the number apart and assign them to array*/
+	while (numb != 0)
+	{
+		my_arr[i] = (numb % 10);
+		numb = numb / 10;
+		if (numb == 0)
+		{
+			break;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	/*Write the content of the array to stdout*/
+	for (i; i >= 0; i--)
+	{
+		/*Convert int to char*/
+		x[0] = ('0' + my_arr[i]);
+		count += write(1, x, 1);
+	}
+	return (count);
 }
--- print_str.c
+++ print_str.c
@@ -1,26 +1,25 @@
 #include "main.h"
+
 /**
- * print_str - print a varaidic string each character per time to stdout
+ * print_str - print a variadic string each character per time to stdout
  * @arguments: variadic parameter
  * Return: number of character
 */
-
 int print_str(va_list arguments)
 {
-int i;
-int count = 0;
-char *str;
+	int i;
+	int count = 0;
+	char *str;
 
-str = va_arg(arguments, char *);
-if (str == NULL)
-{
-return (-1);
-}
-while (str[i])
-{
-count = write(1, &str[i], 1);
-i = i + 1;
-}
-
-return (count);
+	str = va_arg(arguments, char *);
+	if (str == NULL)
+	{
+		return (-1);
+	}
+	while (str[i])
+	{
+		count = write(1, &str[i], 1);
+		i = i + 1;
+	}
+	return (count);
 }
